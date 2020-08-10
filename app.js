8131d358c07bf8d7765472cc064557b7a16cdcb1
akshatadsule <akshat.adsule@gmail.com>
2020-08-10 12:29:06 -0700

    add mongodb boilerplate

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 require('ejs');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', function(req, res) {
+//akshat key: Zqz3AGKJw5iZAoft
+
+mongoose.connect('mongodb+srv://akshat:Zqz3AGKJw5iZAoft@lokal.etj61.gcp.mongodb.net/lokal?retryWrites=true&w=majority');
+
+const userSchema = mongoose.Schema({
+    name: String,
+    password: String,
+    zipCode: Number
+});
+
+const User = new mongoose.model('user', userSchema);
+
+app.get('/', function (req, res) {
     res.render('welcome');
 });
 
-app.listen(7000, function() {
+app.post('register', function(req, res) {});
+
+app.listen(7000, function () {
     console.log('app is running on 7000.');
 });
