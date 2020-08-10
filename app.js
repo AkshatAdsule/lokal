585c266773bb3abbcf6bef33316e06f0590ea807
akshatadsule <akshat.adsule@gmail.com>
2020-08-09 22:40:51 -0700

    Create welcome page

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser');
 require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('welcome');
+});
 
 app.listen(7000, function() {
     console.log('app is running on 7000.');
-})
+});
