1082637d682381ddd66f7a735b2b03bf4137386e
akshatadsule <akshat.adsule@gmail.com>
2020-08-09 22:10:06 -0700

    Add logo + stub code for server

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+require('ejs');
 
 const app = express();
 
---
+++ assets/README.md
@@ -0,0 +1,3 @@
+# Guidlines
+- Font: Open Sans
+- Background Color: rgb(206,255, 119); #ceff77.
